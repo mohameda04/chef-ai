0525a0aa7a68c6b92cb368e4f21baf36b0e4aac2
Mohamed Abdulmalik <mma6599@psu.edu>
2025-12-01 12:48:46 +0300

    Implemented useState hook to automatically re-render on ingredient updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react"
+
 export default function Main() {
-  return (
-    <main>
-      <h1>Welcome to Chef AI</h1>
-      <p>Your personal AI-powered cooking assistant.</p>
-      <form className="ingredient-form"> 
-        <input 
-            type="text"
-            placeholder="e.g. parsley"
-            aria-label="Add ingredient"
-            />
-            <button>Add ingredient</button>
-      </form>
-    </main>
+
+    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(ingredients => [...ingredients, newIngredient])
+    }
+
+    return (
+        <main>
+        <h1>Welcome to Chef AI</h1>
+        <p>Your personal AI-powered cooking assistant.</p>
+        <form onSubmit={handleSubmit} className="ingredient-form"> 
+            <input 
+                type="text"
+                placeholder="e.g. parsley"
+                aria-label="Add ingredient"
+                name="ingredient"
+                />
+                <button>Add ingredient</button>
+        </form>
+        <ul>
+            {ingredientsListItems}
+        </ul>
+        </main>
   )
 }
